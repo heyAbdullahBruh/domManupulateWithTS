00dc622f37a206de514ad203c4b78d40943f3c99
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-09 17:36:25 +0600

    TS code done for dom manupolation

--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,17 @@
 console.log(`Hellow`);
 
+const uForm = document.querySelector("#uForm") as HTMLFormElement;
+const uName = document.querySelector("#name") as HTMLInputElement;
+const uEmail = document.querySelector("#email") as HTMLInputElement;
+const uPhone = document.querySelector("#phone") as HTMLInputElement;
+const message = document.querySelector("#message") as HTMLInputElement;
 
+uForm.addEventListener("submit", (e: Event) => {
+  e.preventDefault();
+  console.log({
+    name: uName.value,
+    email: uEmail.value,
+    phone: uPhone.value,
+    message: message.value,
+  });
+});
