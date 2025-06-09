b79596303d8a1752b3acd6e70d3119c68c3e4bb6
heyAbdullahBruh <mr.sayedf@yandex.com>
2025-06-09 17:37:01 +0600

    JS code configerd

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,16 @@
 "use strict";
 console.log(`Hellow`);
-
+const uForm = document.querySelector("#uForm");
+const uName = document.querySelector("#name");
+const uEmail = document.querySelector("#email");
+const uPhone = document.querySelector("#phone");
+const message = document.querySelector("#message");
+uForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    console.log({
+        name: uName.value,
+        email: uEmail.value,
+        phone: uPhone.value,
+        message: message.value,
+    });
+});
